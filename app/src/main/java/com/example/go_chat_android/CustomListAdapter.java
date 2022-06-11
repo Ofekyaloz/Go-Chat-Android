0edb68e9de1a97ec5b35070e50050c5ade4c8f1b
ofek1 <ofekyaloz16@gmail.com>
2022-06-11 11:37:39 +0300

    classes, new activities and more ...

--- app/src/main/java/com/example/go_chat_android/CustomListAdapter.java
+++ app/src/main/java/com/example/go_chat_android/CustomListAdapter.java
@@ -5,21 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.example.go_chat_android.entities.User;
+import com.example.go_chat_android.entities.Contact;
 
 import java.util.ArrayList;
 
-public class CustomListAdapter extends ArrayAdapter<User> {
+public class CustomListAdapter extends ArrayAdapter<Contact> {
     LayoutInflater inflater;
 
-    public CustomListAdapter(Context ctx, ArrayList<User> userArrayList) {
-        super(ctx, R.layout.custom_list_item, userArrayList);
+    public CustomListAdapter(Context ctx, ArrayList<Contact> contacts) {
+        super(ctx, R.layout.custom_list_item, contacts);
 
         this.inflater = LayoutInflater.from(ctx);
     }
@@ -28,21 +27,21 @@ public class CustomListAdapter extends ArrayAdapter<User> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        User user = getItem(position);
+        Contact contact = getItem(position);
 
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.custom_list_item, parent, false);
         }
 
-        ImageView imageView = convertView.findViewById(R.id.profile_image);
+//        ImageView imageView = convertView.findViewById(R.id.profile_image);
         TextView userName = convertView.findViewById(R.id.user_name);
         TextView lastMsg = convertView.findViewById(R.id.last_massage);
         TextView time = convertView.findViewById(R.id.time);
 
-        imageView.setImageResource(user.getPictureId());
-        userName.setText(user.getUserName());
-        lastMsg.setText(user.getLastMassage());
-        time.setText(user.getLastMassageSendingTime());
+//        imageView.setImageResource(contact.getPictureId());
+        userName.setText(contact.getName());
+        lastMsg.setText(contact.getLast());
+        time.setText(contact.getLastdate());
 
         return convertView;
     }
