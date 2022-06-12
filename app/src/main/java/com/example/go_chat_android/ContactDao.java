21f70ad427ebe518387af1776d04c8b07cec821e
gilig <giligutfeld@gmail.com>
2022-06-12 12:17:40 +0300

    Update

---
+++ app/src/main/java/com/example/go_chat_android/ContactDao.java
@@ -0,0 +1,23 @@
+package com.example.go_chat_android;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    @Query("SELECT * FROM contactsTable")
+    List<Contact> index();
+    @Query("SELECT * FROM contactsTable WHERE name = :name")
+    Contact get(String name);
+    @Insert
+    void insert(Contact... contacts);
+    @Update
+    void update(Contact... contacts);
+    @Delete
+    void delete(Contact... contacts);
+}
